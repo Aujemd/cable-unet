0ce39c08862c1b2c582499536d36b36afe834eb6
Johan Mora <joenmodu99@gmail.com>
2021-03-11 20:07:26 -0430

    Fix bug in redirect on service controller

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -11,7 +11,8 @@
                 <div class="grid grid-cols-3 gap-6">
                     <div class="col-span-6 sm:col-span-3">
                         <label for="type" class="block text-sm font-medium text-gray-700">Tipo</label>
-                        <select id="type" name="type" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <select id="type" name="type"
+                            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                             <option>Cable</option>
                             <option>Internet</option>
                             <option>Telefonía</option>
@@ -20,21 +21,27 @@
                     <div class="col-span-6 sm:col-span-3">
                         <label for="option" class="block text-sm font-medium text-gray-700">Opción</label>
                         <div class="mt-1 flex rounded-md shadow-sm">
-                            <input type="text" name="option" id="option" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Tipo A, 500 segundos, plan quedate en casa ...">
+                            <input type="text" name="option" id="option"
+                                class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                placeholder="Tipo A, 500 segundos, plan quedate en casa ...">
                         </div>
                     </div>
                     <div class="col-span-6 sm:col-span-3">
                         <label for="price" class="block text-sm font-medium text-gray-700">Precio</label>
                         <div class="mt-1 flex rounded-md shadow-sm">
-                            <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                            <span
+                                class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
                                 $
                             </span>
-                            <input type="number" name="price" id="price" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="0">
+                            <input type="number" name="price" id="price"
+                                class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
+                                placeholder="0">
                         </div>
                     </div>
                 </div>
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button type="submit"
+                        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         Guardar
                     </button>
                 </div>
